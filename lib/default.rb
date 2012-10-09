7ee88c693b4d8d2e018a067c5fe0b58f9b40dadc
bjornd <echo.bjornd@gmail.com>
2012-10-09 13:47:41 +0300

    Added blog rendering

--- lib/default.rb
+++ lib/default.rb
@@ -1,2 +1,11 @@
+include Nanoc3::Helpers::Rendering
+
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
+def get_blog_posts
+  return @items.select { |item| item.identifier.index('/blog/') == 0 }.reverse
+end
+
+def get_item_date(item)
+  return Date.parse(item.identifier.split('/').last[0..9])
+end
