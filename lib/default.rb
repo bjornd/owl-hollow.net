d12380260ffcf409f2462dc68f9d3007322cb18a
bjornd <echo.bjornd@gmail.com>
2012-10-11 12:52:19 +0300

    Finished leaks article

--- lib/default.rb
+++ lib/default.rb
@@ -3,7 +3,7 @@ include Nanoc3::Helpers::Rendering
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
 def get_blog_posts
-  return @items.select { |item| item.identifier.index('/blog/') == 0 }.reverse
+  return @items.select { |item| item.identifier.index('/blog/') == 0 && item[:published] }.reverse
 end
 
 def get_item_date(item)
